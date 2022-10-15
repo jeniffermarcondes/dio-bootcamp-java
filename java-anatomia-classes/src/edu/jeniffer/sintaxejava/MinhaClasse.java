5daff31acf5e3d0fbf94c10cb67de4b19b019778
jeniffer.almas <jeniffer.almas@jallcard.com.br>
2022-10-15 13:05:54 -0300

    commit anatomia das classes

--- java-anatomia-classes/src/edu/jeniffer/sintaxejava/MinhaClasse.java
+++ java-anatomia-classes/src/edu/jeniffer/sintaxejava/MinhaClasse.java
@@ -1,16 +1,14 @@
+package edu.jeniffer.sintaxejava;
+
 public class MinhaClasse {
-
     public static void main(String[] args) {
-
         String primeiroNome = "Jeniffer";
         String segundoNome = "Marcondes";
-
         String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
         System.out.println(nomeCompleto);
     }
 
     public static String nomeCompleto(String primeiroNome, String segundoNome) {
-        // metodos no infinitivo
         return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
     }
 
