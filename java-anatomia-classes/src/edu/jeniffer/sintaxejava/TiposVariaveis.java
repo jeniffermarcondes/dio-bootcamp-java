465cd174d5b36d4aa8958e2873fa6ea0a8ce0d97
jeniffer.almas <jeniffer.almas@jallcard.com.br>
2022-10-15 15:06:13 -0300

    tipos e variaveis

---
+++ java-anatomia-classes/src/edu/jeniffer/sintaxejava/TiposVariaveis.java
@@ -0,0 +1,22 @@
+package edu.jeniffer.sintaxejava;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        // constante
+
+        // int numero = 1;
+        // numero = 10;
+        // System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
+
+    }
+
+}
