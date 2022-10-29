1028750df6a47b64df7936bf0dedb6399e44a3c1
jeniffer.almas <jeniffer.almas@jallcard.com.br>
2022-10-29 12:55:13 -0300

    lógica e controle de fluxo

---
+++ java-anatomia-classes/src/edu/jeniffer/logicacondicionalecontroledefluxo/Blocos.java
@@ -0,0 +1,19 @@
+package edu.jeniffer.logicacondicionalecontroledefluxo;
+
+public class Blocos {
+
+    /*
+     * BLOCOS:
+     * é um grupo de 0 ou mais códigos que trabalham em conjunto para executar uma
+     * operação
+     * 
+     * Tipos:
+     * Locais - Dentro de métodos
+     * {
+     * ...
+     * }
+     * Estáticos - Dentro de classes
+     * Instância - Dentro de classes
+     */
+
+}
